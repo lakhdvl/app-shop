6f1489a1188077320dcf0d8c2194cf3e7feb1c23
lakhdvl <130720079+lakhdvl@users.noreply.github.com>
2023-04-14 19:33:39 +0700

    App-shop-push-first-commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\Admin\MainController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/admin/users/login', [LoginController::class, 'index']);
+Route::post('/admin/users/login/store', [LoginController::class, 'store']);
+
+Route::get('/admin/main', [MainController::class, 'index'])->name('admin');
